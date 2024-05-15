8c1558824a0876baf2ef31fdc0a31318049417bd
Rodrigo Valim <rodrigovalimjr2011@hotmail.com>
2024-05-15 16:17:55 -0300

    Path params

---
+++ pathParamsETratamentoDeExcecao/src/main/java/com/rodrigovalim07/MathController.java
@@ -0,0 +1,22 @@
+package com.rodrigovalim07;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MathController {
+
+	private final AtomicLong counter = new AtomicLong();
+	
+	@RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+	public Double greeting(
+			@PathVariable(value = "numberOne") String numberOne,
+			@PathVariable(value = "numberTwo") String numberTwo
+			) {
+		return 1D;
+	}
+}
