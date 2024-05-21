bdfc8d7d9f9c0c3a5c187e79c26833c83aac6feb
Rodrigo Valim <rodrigovalimjr2011@hotmail.com>
2024-05-21 15:14:49 -0300

    Migrations com Flyway

---
+++ migrationsWithFlyway/src/test/java/com/rodrigovalim07/StartupTests.java
@@ -0,0 +1,13 @@
+package com.rodrigovalim07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
