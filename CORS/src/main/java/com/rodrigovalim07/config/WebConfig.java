f648dc186414e4652da0eb0f860769e7e0abe13d
Rodrigo Valim <rodrigovalimjr2011@hotmail.com>
2024-05-25 12:36:03 -0300

    Habilitando CORS de Forma Global

--- CORS/src/main/java/com/rodrigovalim07/config/WebConfig.java
+++ CORS/src/main/java/com/rodrigovalim07/config/WebConfig.java
@@ -2,10 +2,12 @@ package com.rodrigovalim07.config;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.rodrigovalim07.serialization.converter.YamlJackson2HttpMessageConverter;
@@ -15,11 +17,23 @@ public class WebConfig implements WebMvcConfigurer {
 
 	private static final MediaType MEDIA_TYPE_APPLICATION_YAML = MediaType.valueOf("application/x-yaml");
 	
+	@Value("${cors.originPatterns:default}")
+	private String corsOriginPatterns = "";
+	
 	@Override
 	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
 		converters.add(new YamlJackson2HttpMessageConverter());
 	}
 	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		var allowedOrigins = corsOriginPatterns.split(",");
+		registry.addMapping("/**")
+		.allowedMethods("*")
+		.allowedOrigins(allowedOrigins)
+		.allowCredentials(true);
+	}
+
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
 		
