72bc9175c0f733f730884a30d00bdae909092824
Rodrigo Valim <rodrigovalimjr2011@hotmail.com>
2024-05-27 16:21:56 -0300

    Implementando JwtTokenProvider

---
+++ JWTeSpringSecurity/src/main/java/com/rodrigovalim07/securityJwt/JwtTokenProvider.java
@@ -0,0 +1,87 @@
+package com.rodrigovalim07.securityJwt;
+
+import java.util.Base64;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.rodrigovalim07.data.vo.v1.security.TokenVO;
+
+import jakarta.annotation.PostConstruct;
+
+@Service
+public class JwtTokenProvider {
+
+	@Value("${security.jwt.token.secret-key:secret}")
+	private String secretKey = "secret";
+	
+	@Value("${security.jwt.token.expire-lenght:3600000}")
+	private long validityInMilliseconds = 3600000; // 1h
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	Algorithm algotithm = null;
+	
+	@PostConstruct
+	protected void init() {
+		secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+		algotithm = Algorithm.HMAC256(secretKey.getBytes());
+	}
+	
+	public TokenVO createAccessToken(String username, List<String> roles) {
+		Date now = new Date();
+		Date validity = new Date(now.getTime() + validityInMilliseconds);
+		var accesssToken = getAccessToken(username, roles, now, validity);
+		var refreshToken = getRefreshToken(username, roles, now);
+		return new TokenVO(username, true, now, validity, accesssToken, refreshToken);
+	}
+
+	private String getAccessToken(String username, List<String> roles, Date now, Date validity) {
+		String issuerUrl = ServletUriComponentsBuilder.fromCurrentContextPath().build().toString();
+		return JWT.create()
+				.withClaim("roles", roles)
+				.withIssuedAt(now)
+				.withExpiresAt(validity)
+				.withSubject(username)
+				.withIssuer(issuerUrl)
+				.sign(algotithm)
+				.strip();
+	}
+	
+	private String getRefreshToken(String username, List<String> roles, Date now) {
+		Date validityRefreshToken = new Date(now.getTime() + (validityInMilliseconds * 3));
+		return JWT.create()
+				.withClaim("roles", roles)
+				.withIssuedAt(now)
+				.withExpiresAt(validityRefreshToken)
+				.withSubject(username)
+				.sign(algotithm)
+				.strip();
+	}
+	
+	public Authentication getAuthentication(String token) {
+		DecodedJWT decodedJWT = decodedToken(token);
+		UserDetails userDetails = this.userDetailsService.loadUserByUsername(decodedJWT.getSubject());
+		return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+	}
+
+	private DecodedJWT decodedToken(String token) {
+		Algorithm alg = Algorithm.HMAC256(secretKey.getBytes());
+		JWTVerifier verifier = JWT.require(alg).build();
+		DecodedJWT decodedJWT = verifier.verify(token);
+		return decodedJWT;
+	}
+}
