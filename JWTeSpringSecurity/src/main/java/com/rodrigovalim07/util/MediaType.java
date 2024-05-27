fcad11d909703442847ba2f39c91c245ed7da9ce
Rodrigo Valim <rodrigovalimjr2011@hotmail.com>
2024-05-27 12:21:24 -0300

    Ajustando a Base de Dados Para Suportar Autenticação

---
+++ JWTeSpringSecurity/src/main/java/com/rodrigovalim07/util/MediaType.java
@@ -0,0 +1,9 @@
+package com.rodrigovalim07.util;
+
+public class MediaType {
+
+	public static final String APPLICATION_JSON = "application/json";
+	public static final String APPLICATION_XML = "application/xml";
+	public static final String APPLICATION_YAML = "application/x-yaml";
+
+}
---
+++ JWTeSpringSecurity/src/test/java/com/rodrigovalim07/integrationtests/swagger/SwaggerIntegrationTests.java
@@ -0,0 +1,31 @@
+package com.rodrigovalim07.integrationtests.swagger;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.rodrigovalim07.configs.TestConfigs;
+import com.rodrigovalim07.integrationtests.testcontainers.AbstractIntegrationTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class SwaggerIntegrationTests extends AbstractIntegrationTest {
+
+	@Test
+	public void shouldDisplaySwaggerUiPage() {
+		var content =
+			given()
+				.basePath("/swagger-ui/index.html")
+				.port(TestConfigs.SERVER_PORT)
+				.when()
+					.get()
+				.then()
+					.statusCode(200)
+				.extract()
+					.body()
+						.asString();
+		assertTrue(content.contains("Swagger UI"));
+	}
+
+}
