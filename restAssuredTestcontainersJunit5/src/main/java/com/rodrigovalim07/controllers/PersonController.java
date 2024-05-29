b74a09e590327356206b4a58d5ec8940dbc87c14
Rodrigo Valim <rodrigovalimjr2011@hotmail.com>
2024-05-29 15:02:14 -0300

    Fixando os Testes Quebrados

---
+++ restAssuredTestcontainersJunit5/src/main/java/com/rodrigovalim07/controllers/PersonController.java
@@ -0,0 +1,101 @@
+package com.rodrigovalim07.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rodrigovalim07.data.vo.v1.PersonVO;
+import com.rodrigovalim07.services.PersonServices;
+import com.rodrigovalim07.util.MediaType;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+// @CrossOrigin
+@RestController
+@RequestMapping("/api/person/v1")
+@Tag(name = "People", description = "Endpoints for Managing people")
+public class PersonController {
+
+	@Autowired
+	private PersonServices service;
+	
+	@GetMapping(produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML})
+	@Operation(summary = "Finds all people", description = "Finds all people", tags = {"People"},
+	responses = {@ApiResponse(description = "Success", responseCode = "200", content = {
+			@Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = PersonVO.class)))}),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+			@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)})
+	public List<PersonVO> findAll() {
+		return service.findAll();
+	}
+	
+	
+	@CrossOrigin(origins = "http://localhost:8080")
+	@GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML})
+	@Operation(summary = "Finds a person", description = "Finds a person", tags = {"People"},
+	responses = {@ApiResponse(description = "Success", responseCode = "200", content = 
+			@Content(schema = @Schema(implementation = PersonVO.class))),
+			@ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+			@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)})
+	public PersonVO findById(@PathVariable(value = "id") Long id) {
+		return service.findById(id);
+	}
+	
+	@CrossOrigin(origins = {"http://localhost:8080", "https://rodrigovalim07.com"} )
+	@PostMapping(consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML},
+				 produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML})
+	@Operation(summary = "Adds a new person", description = "Adds a new person by passing in a JSON, XML OR YML representation of the person",
+	tags = {"People"}, responses = {@ApiResponse(description = "Success", responseCode = "200", content = 
+			@Content(schema = @Schema(implementation = PersonVO.class))),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)})
+	public PersonVO create(@RequestBody PersonVO person) {
+		return service.create(person);
+	}
+	
+	@PutMapping(consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML},
+				produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML})
+	@Operation(summary = "Updates a person", description = "Updates a person by passing in a JSON, XML OR YML representation of the person",
+	tags = {"People"}, responses = {@ApiResponse(description = "Update", responseCode = "200", content = 
+			@Content(schema = @Schema(implementation = PersonVO.class))),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+			@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)})
+	public PersonVO update(@RequestBody PersonVO person) {
+		return service.update(person);
+	}
+	
+	@DeleteMapping("/{id}")
+	@Operation(summary = "Deletes a person", description = "Deletes a person", tags = {"People"},
+	responses = {@ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+			@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)})
+	public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+}
---
+++ restAssuredTestcontainersJunit5/src/main/resources/db/migration/V8__Insert_Data_In_Users.sql
@@ -0,0 +1,3 @@
+﻿INSERT INTO `users` (`user_name`, `full_name`, `password`, `account_non_expired`, `account_non_locked`, `credentials_non_expired`, `enabled`) VALUES
+	('leandro', 'Leandro Costa', '1e3cdeeaaaeeda173ff6d002e7cb5e3f91ebc354dcff52156c9eaba1793a3a5e5bee306c11099e22', b'1', b'1', b'1', b'1'),
+	('flavio', 'Flavio Costa', '75ec349c1b0ef4ee7b249d0b83ae4861853f3aa77bce8c4b15f28cd43c6424ab4f29df431831bb0d', b'1', b'1', b'1', b'1');
