f0fbc1dd1a21ed4ac83c600e2653d1e3d54179f2
Rodrigo Valim <rodrigovalimjr2011@hotmail.com>
2024-06-03 11:23:59 -0300

    Implementando Paginação para a Operação de FindAll Books

--- queryParamsBuscaPaginada/src/main/java/com/rodrigovalim07/controllers/PersonController.java
+++ queryParamsBuscaPaginada/src/main/java/com/rodrigovalim07/controllers/PersonController.java
@@ -1,9 +1,12 @@
 package com.rodrigovalim07.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -45,9 +48,14 @@ public class PersonController {
 			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
 			@ApiResponse(description = "Not found", responseCode = "404", content = @Content),
 			@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)})
-	public ResponseEntity<Page<PersonVO>> findAll(@RequestParam(value = "page", defaultValue = "0") Integer page,
-												  @RequestParam(value = "limit", defaultValue = "12") Integer limit) {
-		Pageable pageable = PageRequest.of(page, limit);
+	public ResponseEntity<PagedModel<EntityModel<PersonVO>>> findAll(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "size", defaultValue = "12") Integer size,
+			@RequestParam(value = "direction", defaultValue = "asc") String direction) {
+		
+		var sortDirection = "desc".equalsIgnoreCase(direction) ? Direction.DESC : Direction.ASC;
+		
+		Pageable pageable = PageRequest.of(page, size, Sort.by(sortDirection, "firstName"));
 		return ResponseEntity.ok(service.findAll(pageable));
 	}
 	
--- queryParamsBuscaPaginada/src/main/java/com/rodrigovalim07/services/BookServices.java
+++ queryParamsBuscaPaginada/src/main/java/com/rodrigovalim07/services/BookServices.java
@@ -3,10 +3,14 @@ package com.rodrigovalim07.services;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
-import java.util.List;
 import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.stereotype.Service;
 
 import com.rodrigovalim07.controllers.BookController;
@@ -25,13 +29,20 @@ public class BookServices {
 	@Autowired
 	BookRepository repository;
 	
-	public List<BookVO> findAll() {
+	@Autowired
+	PagedResourcesAssembler<BookVO> assembler;
+	
+	public PagedModel<EntityModel<BookVO>> findAll(Pageable pageable) {
 		logger.info("Finding all book...");
 		
-		var books = DozerMapper.parseListObjects(repository.findAll(), BookVO.class);
-		books.stream()
-		.forEach(p -> p.add(linkTo(methodOn(BookController.class).findById(p.getKey())).withSelfRel()));
-		return books;
+		var bookPage = repository.findAll(pageable);
+		
+		var bookVosPage = bookPage.map(p -> DozerMapper.parseObject(p, BookVO.class));
+		bookVosPage.map(p -> p.add(linkTo(methodOn(BookController.class).findById(p.getKey())).withSelfRel()));
+		
+		Link link = linkTo(methodOn(BookController.class)
+				.findAll(pageable.getPageNumber(), pageable.getPageSize(), "asc")).withSelfRel();
+		return assembler.toModel(bookVosPage, link);
 	}
 
 	public BookVO findById(Long id) {
